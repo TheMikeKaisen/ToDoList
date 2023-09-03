120099e06056a4f059c5301b8edad39c9ae42840
TheMikeKaisen <98148929+TheMikeKaisen@users.noreply.github.com>
2023-09-03 10:11:47 +0530

    first client commit

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,27 @@
+import {useState} from "react";
+
+const TodoForm = () => {
+
+
+    // state
+    const [text, setText] = useState('Hello');
+
+    const onFormSubmit = () =>{
+        
+    }
+
+    const onInputChange = (e) => {
+        setText(e.target.value);
+    }
+    return (
+        <form className="form" onSubmit = {onFormSubmit}>
+            <input 
+                placeholder ="Enter new todo..."
+                className = "input"
+                onChange = {onInputChange}
+            />
+        </form>
+    )
+}
+
+export default TodoForm;
